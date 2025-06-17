763bac72071a78123f17cb28fedc158eb76529ee
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2025-06-18 01:42:10 +0700

    fix

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-from io import BytesIO, StringIO
+import codecs
+from io import StringIO, BytesIO
 
 from fastapi import FastAPI, UploadFile, HTTPException
 import pandas as pd
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:4321"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -16,43 +31,106 @@ async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
 
-@app.post("/gpa")
-async def gpa_calculator(file: UploadFile):
+async def read_file(file: UploadFile):
     if not file.filename.endswith("xls"):
         raise HTTPException(status_code=400, detail="<UNK>")
 
     # Reading and decode xls file
     data = await file.read()
-    data = data.decode("utf-16")
-    data = pd.read_html(StringIO(data))[0]
-    data = data.to_dict(orient="records")
+    data = pd.read_excel(BytesIO(data), engine="xlrd")
+    data = data.dropna(axis=0, how='all')  # Drop rows where all elements are NaN
+    return data.to_dict(orient="records")
 
-    print(data[10])
 
+async def cal_gpa(data: list):
     total_points = 0
     total_credits = 0
-    not_done_rows = 0
-    not_counted_rows_semester_subject_code = [
-        "LUK1",
-        "LUK2",
-        "LUK3",
-        "LUK4",
-        "TRS501",
-        "(*) Môn điều kiện tốt nghiệp, không tính vào điểm trung bình tích lũy"
-    ]
     for row in data:
-        is_do_not_count = True if row[('Unnamed: 10_level_0', 'Unnamed: 10_level_1')] == "*" else False
+        is_do_not_count = True if row['Unnamed: 10'] == "*" else False
         if not is_do_not_count:
-            is_passed = True if row[('Status', 'Unnamed: 9_level_1')] == 'Passed' else False
+            is_passed = True if row['Status'] == 'Passed' else False
             if not is_passed:
-                if not row[('Semester', 'SubjectCode')] in not_counted_rows_semester_subject_code:
-                    not_done_rows += 1
                 continue
-            grade = row[('Grade', 'Unnamed: 8_level_1')]
-            credit = row[('Credit', 'Unnamed: 7_level_1')]
+            grade = row['Grade']
+            credit = row['Credit']
             total_credits += float(credit)
             total_points += float(grade) * float(credit)
 
     result = round(total_points / total_credits, 2)
+    return {
+        "result": result,
+        "total_credits": total_credits,
+        "total_points": total_points,
+    }
 
-    return {"gpa": result}
+
+@app.post("/gpa")
+async def gpa_calculator(file: UploadFile):
+    # Reading and decode xls file
+    data = await read_file(file)
+
+    # Calculating GPA
+    gpa_rs = await cal_gpa(data)
+
+    return {
+        "gpa": gpa_rs["result"],
+    }
+
+
+@app.post("/gpa/target")
+async def gpa_target_calculator(file: UploadFile, target_gpa):
+    data = await read_file(file)
+
+    gpa_rs = await cal_gpa(data)
+    total_gpa = gpa_rs["result"]
+    target_gpa = float(target_gpa)
+    if target_gpa < total_gpa:
+        raise HTTPException(status_code=400, detail="Target GPA must be greater than current GPA")
+
+    total_credits = gpa_rs["total_credits"]
+    total_points = gpa_rs["total_points"]
+    remain_credits = 0
+    largest_no = 0
+
+    not_counted_rows_subject_code = [
+        "LUK Global 1",
+        "LUK Global 2",
+        "LUK Global 3",
+        "LUK Global 4",
+        "English 5 (University success)",
+    ]
+
+    for row in data:
+        is_no_valid = False if row['No'] == "(*) Môn điều kiện tốt nghiệp, không tính vào điểm trung bình tích lũy" or \
+                               row['No'] == "nan" or \
+                               row['No'] == "No" else True
+        is_do_not_count = True if row['Unnamed: 10'] == "*" else False
+        if not is_do_not_count and is_no_valid:
+            if float(row['No']) > largest_no:
+                largest_no = float(row['No'])
+
+        print(largest_no)
+
+    for row in data:
+        is_do_not_count = True if row['Unnamed: 10'] == "*" else False
+        is_not_counted_rows_semester_subject_code = True if row[
+                                                                'Subject Code'] in not_counted_rows_subject_code else False
+        if not is_do_not_count and not is_not_counted_rows_semester_subject_code:
+            is_passed = True if row['Status'] == 'Passed' else False
+            if not is_passed:
+                is_no_valid = False if row[
+                                           'No'] == "(*) Môn điều kiện tốt nghiệp, không tính vào điểm trung bình tích lũy" or \
+                                       row['No'] == "No" else True
+                if is_no_valid:
+                    if float(row['No']) == largest_no:
+                        print(row)
+                        remain_credits += 10
+                    else:
+                        print(row)
+                        remain_credits += 3
+
+    avg_target = (target_gpa * (total_credits + remain_credits) - total_points) / remain_credits
+
+    return {
+        "avg_target": round(avg_target, 2),
+    }
